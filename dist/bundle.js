3e33dd04930641016b90c9d9f057ede12769798e
Tariqul Islam <tariq10043@gmail.com>
2021-08-20 10:49:06 +0600

    typescript webpack quick start pack

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,12 +1,33 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-var __webpack_exports__ = {};
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./app/index.ts":
 /*!**********************!*\
   !*** ./app/index.ts ***!
   \**********************/
+/***/ (() => {
 
-console.log("Hello World!");
+eval("\r\nconsole.log(\"Hello World!\");\r\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9hcHAvaW5kZXgudHMuanMiLCJtYXBwaW5ncyI6IkFBQWE7QUFDYiIsInNvdXJjZXMiOlsid2VicGFjazovL3R5cGVzY3JpcHQtYmFzaWMvLi9hcHAvaW5kZXgudHM/NzIwZiJdLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjtcclxuY29uc29sZS5sb2coXCJIZWxsbyBXb3JsZCFcIik7XHJcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./app/index.ts\n");
 
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./app/index.ts"]();
+/******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=bundle.js.map
